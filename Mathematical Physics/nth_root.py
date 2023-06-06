3043a038653c544b691d3352b210991caa0a4238
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-06-06 11:43:52 +0530

    comments

--- Mathematical Physics/nth_root.py
+++ Mathematical Physics/nth_root.py
@@ -1,5 +1,7 @@
 def func(x,n = 4,A = 81):
-    return x**n - A
+    return x**n - A     #A is the given number and n is the reciprocal power of A 
+
+
 
 def bisection(func, a,b, tolerance = 1e-5, max_iterations = 100):
 
