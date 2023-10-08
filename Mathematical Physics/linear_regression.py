6c1d3ee385384417e17482dfd2d8d1b52ac288f0
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-10-08 12:27:48 +0530

    Update linear_regression.py

--- Mathematical Physics/linear_regression.py
+++ Mathematical Physics/linear_regression.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import linregress
 #linear regression is the linear approximation of a casual relationship between 2 variables
-def linear_relationship(x):
-    return 3*x + 4                #defined the linear expression ŷ = b₀ + b₁x₁
-
+              
 x = np.array([1,2,3,4,5])   #sample data
-y = linear_relationship(x)    #dependent variable
+y = np.array([3,4,6,7,8])    #ŷ = b₀ + b₁x₁
 
 #calculating the linear least-squares regression for the two sets of measurements.
 slope, intercept, r_value, p_value, error = linregress(x,y)   #this module function returns slope, intercept, r value, p value and standard error of a linear relationship
